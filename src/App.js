e9a751aab2c1e787f3d55c9c8ede3c4c1484312d
Samel Mendes <75790706+pysmendes@users.noreply.github.com>
2023-05-19 01:01:21 -0300

    Alteração do projeto react1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
 function App() {
-  const codename= 'Alphabet'
-
-  const newCodename = codename.toUpperCase()
-
-  function sum(a, b) {
-    return a + b
-  }
-
-  const url = 'https://picsum.photos/200'
-  // const urlplaceholder = 'https://via.placeholder.com/150'
-
 
   return (
     <div className="App">
-      <h2>Alteração JSX</h2>
-      <p>Projeto entitulado de {newCodename}</p>
-      <p>Versão : 2023a{sum(0,1)}</p>
-      <img src={url} alt="Imagem gerada"/>
       <HelloWorld />
     </div>
   );
