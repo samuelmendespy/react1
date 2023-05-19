3a3f15c3e88040e1192c1931d7211ea1ac673ebf
Samel Mendes <75790706+pysmendes@users.noreply.github.com>
2023-05-19 01:14:40 -0300

    Alteração de componentes do projeto react1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+
+
 function App() {
 
+  const nome = "Corporation"
   return (
     <div className="App">
       <HelloWorld />
+      <SayMyName nome="Alphabet" />
+      <SayMyName nome="ACME" />
+      <SayMyName nome={nome} />
+      <Pessoa 
+      nome="Megacorp" 
+      idade="3" 
+      profissao="Programador" 
+      foto="https://picsum.photos/150"
+      />
+
     </div>
   );
 }
---
+++ src/components/Pessoa.js
@@ -0,0 +1,11 @@
+function Pessoa() {
+    return <div>
+        <img src={props.foto} alt="Legenda da imagem"/>
+        <h2> Nome: {props.nome}</h2>
+        <h2> Idade: {props.idade}</h2>
+        <h2> Profissão: {props.profissao}</h2>
+
+    </div>
+}
+
+export default Pessoa
