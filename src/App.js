2dc1335f1d8f91915c0eb2391745d2f2c58dd20b
Samel Mendes <75790706+pysmendes@users.noreply.github.com>
2023-05-19 00:32:49 -0300

    Alteração do arquivo App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const codename= 'Alphabet'
+
+  const newCodename = codename.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://picsum.photos/200'
+  // const urlplaceholder = 'https://via.placeholder.com/150'
+
+
   return (
     <div className="App">
-      <h1>Projeto React</h1>
-      <p>Meu projeto react1</p>
+      <h2>Alteração JSX</h2>
+      <p>Projeto entitulado de {newCodename}</p>
+      <p>Versão : 2023a{sum(0,1)}</p>
+      <img src={url} alt="Imagem gerada"/>
     </div>
   );
 }
